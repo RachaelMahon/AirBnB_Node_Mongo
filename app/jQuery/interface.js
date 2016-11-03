a3be86dbd61928c8cc9fb2aba6e64b9ae8058071
RachaelMahon <rachmahon@gmail.com>
2016-11-03 23:34:10 +0000

    Mongo db added, user sign in and sign up created, profile page started

---
+++ app/jQuery/interface.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  var prop = new Properties();
+  var array = prop.listAvailableProperties();
+
+  $('#bowl').click(function () {
+    document.getElementById('demo').innerHTML = $.map(array, function(value) {
+      return( '<br>' + value );
+  });
+  });
+
+  $(window).load(function(){
+  $('.scroll').fadeIn();
+
+});
+
+$('#login-button').click(function(){
+  $('#login-button').fadeOut("slow",function(){
+    $("#container").fadeIn();
+    TweenMax.from("#container", .4, { scale: 0, ease:Sine.easeInOut});
+    TweenMax.to("#container", .4, { scale: 1, ease:Sine.easeInOut});
+  });
+});
+
+$(".close-btn").click(function(){
+  TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
+  TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
+  $("#container, #forgotten-container").fadeOut(800, function(){
+    $("#login-button").fadeIn(800);
+  });
+});
+
+/* Forgotten Password */
+$('#forgotten').click(function(){
+  $("#container").fadeOut(function(){
+    $("#forgotten-container").fadeIn();
+  });
+});
+
+
+
+
+
+
+
+});
